cf92e07b7daa1c4dc6f797404005f9f4ba75768e
Winston Timp <wtimp1@jhmi.edu>
2012-02-17 15:03:44 -0500

    More Versions

--- genetic_region.R
+++ genetic_region.R
@@ -24,42 +24,16 @@ source("~/Work/Analysis/Illumina/genetic_plots1.R")
 source("~/Work/Analysis/Illumina/Illumina_controls1.R")
 ##source("~/Work/Analysis/Illumina/Illumina_sample1b.R")
 
-##Load CSV file
-##Custom set of probes and old set
-probes<-read.csv("New_norm_mat_targer_ucscisl_hmmisl.csv",stringsAsFactors=FALSE)
-old_probes<-read.csv("Old_Illumina_pri_mat_targer_ucscisl_hmmisl.csv",stringsAsFactors=FALSE)
+
+
 ##Load UCSC Isl file
 ucsc_isl<-read.delim("ucsc_cpgisl.txt",stringsAsFactors=FALSE)
 ##Load HMM Isl file
 hmm_isl<-read.csv("hmm_isl1.txt",stringsAsFactors=FALSE)  
 ##Load RefSeq Gene file
 genes<-read.delim("ref_genes.txt",stringsAsFactors=FALSE)
-##Load Data file
-raw<-read.csv("New_norm_mat_data.csv",stringsAsFactors=FALSE)
-raw_old<-read.csv("Old_Illumina_pri_mat_data.csv",stringsAsFactors=FALSE)
-raw2<-read.csv("Second_norm_data_mat_data.csv",stringsAsFactors=FALSE)
 
-
-
-##Get Avg Data
-avg_data<-raw[,seq(2,ncol(raw)-1,by=5)]
-avg_data2<-raw2[,seq(2,ncol(raw2)-1,by=5)]
-old_avg_data<-raw_old[,seq(2,ncol(raw_old)-1,by=5)]
-##Combine runs 1 and 2 of the Custom Array
-fuseavg<-cbind(avg_data,avg_data2)
-##Load Sample file
-sampy2<-read.csv("Second_norm_data_mat_sample.csv",stringsAsFactors=FALSE)
-sampy<-read.csv("New_norm_mat_sample.csv",stringsAsFactors=FALSE)
-##Again - combine runs 1 and 2
-fusesampy=rbind(sampy,sampy2)
-old_sampy<-read.csv("Old_Illumina_pri_mat_sample.csv",stringsAsFactors=FALSE)
-
-##CHARM probe loc load
-##load("CHARM_nfo.rda")
-
-##Load CHARM Data
-##load("CHARM_data.rda")
-
+#Label colors/names?
 class_stuff=data.frame(tissue=c(0,1,rep(2,4), rep(3,5),rep(4,2), rep(5,2), rep(6,2), rep(7,10),rep(8,5)),
   type=c(0,0,-2, -1, 1, 2, -3,-1,1,2,3,-1,1,-1,1,-1,1,-7,-4, -2, 1, 2, 3, 4, 5, 6, 7,-2,-1, 1, 2, 3),
   nam=c("Controls","Cross_controls","Breast DCIS Normal","Breast Normal","Breast Tumor", "Breast DCIS Tumor",
@@ -72,7 +46,11 @@ class_stuff=data.frame(tissue=c(0,1,rep(2,4), rep(3,5),rep(4,2), rep(5,2), rep(6
     rep("firebrick1", 3), rep("firebrick4", 7), rep("lightsteelblue1", 2), rep("lightsteelblue4", 3)),stringsAsFactors=FALSE)
 
 
-##Plot new data all sets
+##CHARM probe loc load
+##load("CHARM_nfo.rda")
+
+##Load CHARM Data
+##load("CHARM_data.rda")
 
 ##plotsubdTN(probes,genes,avg_data,sampy,"Movie/full_big_hist.pdf")
 
