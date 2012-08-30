c51fb2158f66da1ac5f642d5e9ff03db863a960b
Winston Timp <wtimp1@jhmi.edu>
2012-08-30 19:50:47 -0400

    Cleaned up a file, started vmr.find

--- 450k_timp_functions.R
+++ 450k_timp_functions.R
@@ -551,7 +551,7 @@ dmr.find <- function(dat, ccomp="Phenotype", grps=c("normal", "cancer"), MG=500,
 
   type=factor((dat$pd[[ccomp]])[keep],grps)
   
-  ##This is determined sex from data, not sex given from annotation
+  ##This is sex info (either annotation or 
   sex=factor(dat$pd$sex[keep],c("M","F"))
   X=model.matrix(~type+sex)
 
@@ -564,6 +564,78 @@ dmr.find <- function(dat, ccomp="Phenotype", grps=c("normal", "cancer"), MG=500,
 
   ##Get the differential methylation (beta) for summary
   dm=rowMeans(ilogit(y[,type==grps[1]]))-rowMeans(ilogit(y[,type==grps[2]]))
+
+  ##Fit linear model, get out differences per block
+  fit=lmFit(y,X)
+  eb=ebayes(fit)
+  
+  ##Use t-statistic of difference for region finding in this case, could also have used
+  ##fit coef[,2] which is the difference from the linear model fit)
+  ##ss=fit$coef[,2]
+  
+  tab=regionFinder(eb$t[,2], pns, dat$locs$chr, dat$locs$pos, y=dm, cutoff=cutoff, ind=pnsind)
+  ##Need a cr here, regionFinder ... or it looks weird.
+  cat("\n")
+  ##Also - rownames of tab are awfully useless - figure out what the right piece of info is, I think it's probably pns
+  
+  ##Permutation testing for dmr p-value - is this p-value or q-vaule?  Semantics?
+  if (permute.num>0) {
+    cat("Performing", permute.num, "permutations\n")
+    
+    L <- vector("list",permute.num)
+    for(j in 1:permute.num){      
+      cat(j,"")
+      sX=model.matrix(~sample(type)+sex)
+      ##Fit linear model, get out differences per block
+      n.fit=lmFit(y,sX)
+      n.eb=ebayes(n.fit)
+      
+      n.tab=regionFinder(n.eb$t[,2], pns, dat$locs$chr, dat$locs$pos, cutoff=cutoff, ind=pnsind)
+      
+      ##Abs of area for distribution
+      L[[j]]<-abs(n.tab$area)
+      cat(length(L[[j]]),"\n")
+    }
+     
+    l=unlist(L)
+
+    Fn = ecdf(l+1e-9)
+
+    tab$pv = 1-Fn(abs(tab$area))    
+  }
+
+  dmr=GRanges(seqnames=tab$chr, strand="*", range=IRanges(start=tab$start, end=tab$end))
+  values(dmr)=tab[,4:dim(tab)[2]]
+  
+  return(dmr)
+}
+
+vmr.find <- function(dat, ccomp="Phenotype", grps=c("normal", "cancer"), MG=500, MNP=3, cutoff=0.5,
+                     permute.num=0) {
+  ##This function finds VMR, very similiar to dmr.find
+
+  dat=dat.init(dat)
+   
+  ##Select samples that are relevant for VMR finding
+  keep=as.matrix(dat$pd[ccomp])%in%grps
+  y=dat$Y[,keep]
+
+  type=factor((dat$pd[[ccomp]])[keep],grps)
+  
+  ##This is chosen sex, may be annotation, may be from data, depends on dat.preload
+  sex=factor(dat$pd$sex[keep],c("M","F"))
+  X=model.matrix(~type+sex)
+
+  ##Cluster the probes
+  pns=clusterMaker(dat$locs$chr, dat$locs$pos, maxGap=MG)
+  ##Number of probes per cluster
+  Ns=tapply(seq(along=pns), pns, length)
+  ##Find good probe clusters(more than Min number of probes
+  pnsind=which(pns%in%as.numeric(names(which((Ns>MNP)))))
+
+  ##Get the differential methylation (beta) for summary
+  dm=rowMeans(ilogit(y[,type==grps[1]]))-rowMeans(ilogit(y[,type==grps[2]]))
+  s=apply(y,1,mad)
 
   ##Fit linear model, get out differences per block
   fit=lmFit(y,X)
